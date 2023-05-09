ab1acf83a344b7fa63bb168058ab47db6497b0b3
DoooReyn <jl88744653@gmail.com>
2023-05-09 17:29:53 +0800

    draw rectangle in linux

--- Platform/Linux/helloengine_xcb.c
+++ Platform/Linux/helloengine_xcb.c
@@ -5,15 +5,15 @@
 #include <xcb/xcb.h>
 
 int main(void) {
-    xcb_connection_t        *pConn;
-    xcb_screen_t            *pScreen;
-    xcb_window_t            window;
-    xcb_gcontext_t          foreground;
-    xcb_gcontext_t          background;
-    xcb_generic_event_t     *pEvent;
-    uint32_t                mask = 0;
-    uint32_t                values[2];
-    uint8_t                 isQuit = 0;
+    xcb_connection_t	*pConn;
+    xcb_screen_t		*pScreen;
+    xcb_window_t		window;
+    xcb_gcontext_t		foreground;
+    xcb_gcontext_t		background;
+    xcb_generic_event_t	*pEvent;
+    uint32_t		mask = 0;
+    uint32_t		values[2];
+    uint8_t			isQuit = 0;
 
     char title[] = "Hello, Engine!";
     char title_icon[] = "Hello, Engine! (iconified)";
@@ -34,7 +34,7 @@ int main(void) {
     values[1] = 0;
     xcb_create_gc(pConn, foreground, window, mask, values);
 
-    /* create white (background) graphic context */
+    /* create which (background) graphic context */
     background = xcb_generate_id(pConn);
     mask = XCB_GC_BACKGROUND | XCB_GC_GRAPHICS_EXPOSURES;
     values[0] = pScreen->white_pixel;
@@ -46,16 +46,16 @@ int main(void) {
     mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
     values[0] = pScreen->white_pixel;
     values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
-    xcb_create_window (pConn,                                       /* connection */
-                       XCB_COPY_FROM_PARENT,        /* depth */
-                       window,                                      /* window ID */
-                       pScreen->root,                       /* parent window */
-                       20, 20,                                      /* x, y */
-                       640, 480,                            /* width, height */
-                       10,                                          /* boarder width */
+    xcb_create_window (pConn,					/* connection */
+                       XCB_COPY_FROM_PARENT,	/* depth */
+                       window,					/* window ID */
+                       pScreen->root,			/* parent window */
+                       20, 20,					/* x, y */
+                       640, 480,				/* width, height */
+                       10,						/* boarder width */
                        XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class */
-                       pScreen->root_visual,        /* visual */
-                       mask, values);                       /* masks */
+                       pScreen->root_visual,	/* visual */
+                       mask, values);			/* masks */
 
     /* set the title of the window */
     xcb_change_property(pConn, XCB_PROP_MODE_REPLACE, window,
@@ -75,6 +75,11 @@ int main(void) {
     while((pEvent = xcb_wait_for_event(pConn)) && !isQuit) {
         switch(pEvent->response_type & ~0x80) {
             case XCB_EXPOSE:
+            {
+                xcb_rectangle_t rect = { 20, 20, 60, 80 };
+                xcb_poly_fill_rectangle(pConn, window, foreground, 1, &rect);
+                xcb_flush(pConn);
+            }
                 break;
             case XCB_KEY_PRESS:
                 isQuit = 1;
@@ -82,6 +87,8 @@ int main(void) {
         }
         free(pEvent);
     }
+
+    xcb_disconnect(pConn);
 
     return 0;
 }
